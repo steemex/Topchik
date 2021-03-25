f4dff0f20e3cfd3977e0957c797ab3137ce76696
Andrey Sinyagin <steemex@ya.ru>
2021-03-26 00:19:29 +0300

    Initial Commit

---
+++ Topchik/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Topchik
+//
+//  Created by Andrey Sinyagin on 26.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Topchik/TopchikApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopchikApp.swift
+//  Topchik
+//
+//  Created by Andrey Sinyagin on 26.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct TopchikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
